8070c27f5f7df8b3f9ed8961aab979d35851290e
Pranshu Agarwal <pranshu2108@gmail.com>
2022-09-16 23:16:37 +0530

    Google Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './components/Login'
+import Home from './container/Home'
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            hello world
-        </h1>
+        <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+            <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </GoogleOAuthProvider>
     )
 }
 
