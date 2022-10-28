e0f782603f39acee275cf661a8e0e3368de1d51b
Pranshu Agarwal <pranshu2108@gmail.com>
2022-10-28 18:24:37 +0530

    Sidebar And Navbar Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './container/Home'
 
