5dd01e81fa184454bd2383865a30c81035f14718
Prateek Shakya <prateekshakya2906@gmail.com>
2022-10-28 22:48:54 +0530

    implemented image details page

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Rings } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 
 const Spinner = (message) => {
     return (
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <Rings
+            <Circles
                 // type="Circles"
                 color="#00BFFF"
                 height={50}
