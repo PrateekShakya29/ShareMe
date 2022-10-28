32d706c1d868ad04694a6eac2b8072eb55de1a4f
Prateek Shakya <prateekshakya2906@gmail.com>
2022-10-28 20:47:17 +0530

    implemented feed page

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -7,6 +7,7 @@ export const client = sanityClient({
   apiVersion: '2022-09-09',
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
 });
 
 const builder = imageUrlBuilder(client);
